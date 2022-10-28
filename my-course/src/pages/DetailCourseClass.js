939834a7454c845c8a1fca557239fb29b7ef5b0a
kenhardika <kenzamahardika@gmail.com>
2022-10-28 17:01:26 +0700

    update detail course and next task in comment

--- my-course/src/pages/DetailCourseClass.js
+++ my-course/src/pages/DetailCourseClass.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import fetchDetailCourse from '../utils/fetchDetailCourse'
+import VideoCourse from './VideoCourse';
 class DetailCourseClass extends Component {
     constructor(props) {
         super(props);
-        this.state = {data:[]}
+        this.state = {data:[]};
     }
 
     changeState(dat){
@@ -21,21 +22,31 @@ class DetailCourseClass extends Component {
                 user_id
             }
         }} = this.props;
-        const hitAPI = fetchDetailCourse(course_id, user_id);
-        hitAPI.then((data)=> this.changeState(data.data))
+        const responseAPI = fetchDetailCourse(course_id, user_id); // ToDo: async await
+        responseAPI.then((data)=> this.changeState(data.data))
     }
     componentWillUnmount() {
 
     }
 
+    // async checkEachChapters(chap){
+    // return await chap.map((dat)=> console.log(dat));
+    // }
+
     render() {
-        console.log(this.state.data);
-        
+        // console.log(this.state.data);
+        const {data: {title, chapters  } } = this.state; // ToDO: untuk chapters mainin index chapter & lesson
+        console.log(chapters) // todo: class dan function beda repo 
+
+        // this.checkEachChapters(chapters)
+        // chapters.map((data)=>console.log(data));
         return (
             <div>
             <Header></Header>
             <main>
-                This is Detail Course Page!
+                <p>{title}</p>
+                <VideoCourse></VideoCourse>
+                {/* {chapters.map((arr)=><li key={arr.chapter_id}> </li>)} */}
             </main>
             </div>
         );
