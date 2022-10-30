13f46b224378e3de13804dd681e540ce7381093e
kenhardika <kenzamahardika@gmail.com>
2022-10-30 07:52:44 +0700

    Update DetailCourseClass.js destructure lessons into state

--- my-course/src/pages/DetailCourseClass.js
+++ my-course/src/pages/DetailCourseClass.js
@@ -6,29 +6,38 @@ import VideoCourse from './VideoCourse';
 class DetailCourseClass extends Component {
     constructor(props) {
         super(props);
-        this.state = {data:[], current:''};
+        this.state = {  data:{
+                            chapters:[],
+                            title:''
+                            },
+                        lessons:[]
+                        };
+        // this.addLessons= this.addLessons.bind(this);
     }
 
-    changeState(dat){
-        this.setState({
-           data: dat
-        })
-    }
+    // addLessons(lesson){
+    //     this.setState({
+    //         // ...prevState,
+    //         // [lessons]:[value]
+    //         lessons: this.state.lessons.push(lesson)
+    //     });
+    //     // console.log(this.state.lessons)
+    // }
 
     async fetchResponseAPI(course,user){
-        
+
         // if(!responseAPI) return;
         // if(!responseAPI.data.length)return;
         // console.log(responseAPI);
         try{
             const responseAPI = await fetchDetailCourse(course, user); // ToDo: async await
-            const response = responseAPI.data;
+            const response = await responseAPI.data;
             // if(!response.length)return;
             this.setState({
                 data: response
             });
             // console.log(response)
-                // console.log(this.state.dataCourse)    
+                // console.log(this.state.dataCourse)
         }
 
         catch{
@@ -37,22 +46,90 @@ class DetailCourseClass extends Component {
     }
 
     componentDidMount() {
-        // console.log(this.props.match.params.course_id)
         const {match:{
             params:{
                 course_id,
                 user_id
             }
         }} = this.props;
-        
-        
-        this.fetchResponseAPI(course_id,user_id)
-        // fetchResponseAPI();
-        // console.log(mockjson);
-        // this.changeState(mockjson)
+        this.fetchResponseAPI(course_id,user_id);
 
-
+        // const {data:{
+        //     title,
+        //     chapters
+        //     }
+        // } = this.state;
+        // console.log(chapters);
+        // const [arr1, arr2, arr3, arr4, arr5] = chapters;
+        // if(chapters.length>0){
+        //     chapters.map((arr)=>
+        //         arr.lessons.map((lesson)=> {
+        //             // console.log(lesson.link);
+        //             // console.log(lesson.lesson_id);
+        //             // console.log(lesson.title);
+        //             this.setState({
+        //                 lessons: lesson
+        //             });
+        //             // console.log(lesson);
+        //             // change state
+        //         })
+        // )}
     }
+
+    componentDidUpdate(prevProps, prevState){
+        // console.log(prevState);
+        const {data:{chapters}} = prevState;
+        // console.log(chapters);
+        // const [...rest] = chapters
+        // console.log(rest);
+
+        if(this.state.lessons.length){
+            // console.log('something');
+            // console.log(this.state.lessons);
+            return
+        }
+        else if(!this.state.lessons.length){
+            // console.log('nothing');
+            // this.setState({
+            //     lessons: ['something']
+            // })
+            chapters.map((arr)=>
+                arr.lessons.map((lesson)=>
+                // prev = this.state previous, next = this.state kosong
+                this.setState((prev)=>({
+                        lessons: prev.lessons.concat(lesson)
+                    }
+                )))
+            )
+            return
+        }
+
+        // if(chapters.length>0){
+        //     this.setState({
+        //         lessons: ['this is lessons']
+        //     })
+        // }
+
+        // if(chapters.length>0){
+        //     chapters.map((arr)=>
+        //         arr.lessons.map((lesson)=> {
+        //             // console.log(lesson.link);
+        //             // console.log(lesson.lesson_id);
+        //             // console.log(lesson.title);
+        //             this.setState(
+        //                 {
+        //                     lessons: prevState.lessons.concat(lesson)
+        //                 })
+
+        //             // this.setState((prev, next)=>({
+        //             //     lessons: prev.lessons.concat(next)
+        //             // }));
+        //             // console.log(lesson);
+        //             // change state
+        //         })
+        // )}
+    }
+
     componentWillUnmount() {
 
     }
@@ -62,15 +139,57 @@ class DetailCourseClass extends Component {
     // }
 
     render() {
-        // console.log(this.state.data);
+        // console.log(this.state.data.chapters)
+        console.log(this.state.lessons);
+        const {lessons } = this.state.lessons;
+        console.log(lessons)
+    //     const {data:{
+    //         title,
+    //         chapters
+    //     }
+    // } = this.state;
+    // chapters.map(arr=>
+    //     this.setState({
+    //         lessons: this.state.lessons.push(arr)
+    //     })
+    //     )
+        // console.log(chapters);
+        // const [arr1, arr2, arr3, arr4, arr5] = chapters;
+        // console.log(this.state.lessons);
+        // const {data:{
+        //             title,
+        //             chapters
+        //             }
+        //         } = this.state;
+        // console.log(chapters);
+        // // const [arr1, arr2, arr3, arr4, arr5] = chapters;
+        // chapters.map((arr)=>
+        //     arr.lessons.map((lesson)=> {
+        //         console.log(lesson.link);
+        //         console.log(lesson.lesson_id);
+        //         console.log(lesson.title);
+        //         // this.changeState('id', lesson.lesson_id);
+        //         // change state
+        //     })
+        // )
+        // console.log(this.state.lessons);
 
-        if(this.state.data.length){        
-            const{data:{title, chapters}} = this.state;
-            console.log(title);
-            console.log(chapters);
-        }
 
-        
+        // setTimeout(()=>{
+
+            // console.log('udah bagus');
+            // console.log(this.state.data.chapters[0]);
+        // if(this.state.data.length){
+        //     console.log('udah bagus')
+        // }
+        // console.log(this.state.data.chapters)
+        // if(!this.state.data.length){
+            // const{data} = this.state;
+            // console.log(data.chapters);
+            // console.log(chapters);
+        // }
+
+
         // if(chapters.length){
         //     console.log(chapters[0]);
         // }
@@ -79,9 +198,11 @@ class DetailCourseClass extends Component {
         // const {data: {title, chapters:chap } } = this.state; // ToDO: untuk chapters mainin index chapter & lesson
         // console.log(chap);
         // console.log()
-        // todo: class dan function beda repo 
+        // todo: class dan function beda repo
         // this.checkEachChapters(chapters)
         // chapters.map((data)=>console.log(data));
+
+        // }, 300);
         return (
             <div>
             <Header></Header>
@@ -89,15 +210,16 @@ class DetailCourseClass extends Component {
                 <p>{"title"}</p>
                 <VideoCourse></VideoCourse>
                 {/* {chapters.map((arr)=><li key={arr.chapter_id}> </li>)} */}
-                
+
                 <div className="buttonControls">
-                    <button id='nextBtn'>next</button> 
+                    <button id='nextBtn'>next</button>
                     <button id='prevBtn'>prev</button>
                 </div>
             </main>
             </div>
         );
     }
+
 }
 
 export default DetailCourseClass;
